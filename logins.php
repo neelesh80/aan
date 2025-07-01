df1a4b4681de44de1dd2cd40f9c675bf97ed93a2
Aniket123-art <ayushsonisoni49@gmail.com>
2025-07-02 02:45:02 +0530

    Add files via upload

---
+++ logins.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['loggedin'] = true;
+        header("Location: index.php");
+        echo "data inserted";
+    } else {
+        echo "Invalid username or password";
+    }
+  }
+?>
